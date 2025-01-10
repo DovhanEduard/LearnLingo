295ad3320f4dddc74235e570b5509c362e803684
DovhanEduard <145812408+DovhanEduard@users.noreply.github.com>
2025-01-10 17:59:50 +0200

    project settings

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,9 @@
 import { useEffect, lazy, Suspense } from 'react';
-import { Toaster } from 'react-hot-toast';
-import { Route, Routes } from 'react-router-dom';
-import RestrictedRoute from 'components/RestrictedRoute/RestrictedRoute';
-import SharedLayout from 'components/SharedLayout/SharedLayout.jsx';
-import PrivateRoute from 'components/PrivateRoute/PrivateRoute';
 
-const HomePage = lazy(() => import('./pages/HomePage/HomePage'));
-const SignInPage = lazy(() => import('./pages/SignInPage/SignInPage'));
-const SignUpPage = lazy(() => import('./pages/SignUpPage/SignUpPage'));
-const TrackerPage = lazy(() => import('./pages/TrackerPage/TrackerPage'));
+// const HomePage = lazy(() => import('./pages/HomePage/HomePage'));
+// const SignInPage = lazy(() => import('./pages/SignInPage/SignInPage'));
+// const SignUpPage = lazy(() => import('./pages/SignUpPage/SignUpPage'));
+// const TrackerPage = lazy(() => import('./pages/TrackerPage/TrackerPage'));
 
 function App() {
   return (
